5c8237ca34aa767e5dabdd4bec79cd64fae28c07
Michael Sanders <mike.gamedev@gmail.com>
2019-12-09 20:24:54 -0800

    Updated code to work with multiple inputs. Needed to duplicate code input.

--- Day-7b/Program.cs
+++ Day-7b/Program.cs
@@ -9,28 +9,45 @@ namespace Day_7b
     {
         static void Main(string[] args)
         {
-            int[] code = File.ReadAllText(@"day7a-input.txt").Split(",").Select(int.Parse).ToArray();
+            IEnumerable<IEnumerable<int>> permutations = GetPermutations(Enumerable.Range(5, 5), 5);
+            
+            int highestThrust = 0;
 
-            code = new[] { 3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26, 27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5 };
-
-            List<Intcode> amplifiers = new List<Intcode>
+            foreach (var inputs in permutations)
             {
-                new Intcode(code).BufferInput(new List<int>{9}),
-                new Intcode(code).BufferInput(new List<int>{8}),
-                new Intcode(code).BufferInput(new List<int>{7}),
-                new Intcode(code).BufferInput(new List<int>{6}),
-                new Intcode(code).BufferInput(new List<int>{5})
-            };
+                int[] code1 = File.ReadAllText(@"day7a-input.txt").Split(",").Select(int.Parse).ToArray();
+                int[] code2 = File.ReadAllText(@"day7a-input.txt").Split(",").Select(int.Parse).ToArray();
+                int[] code3 = File.ReadAllText(@"day7a-input.txt").Split(",").Select(int.Parse).ToArray();
+                int[] code4 = File.ReadAllText(@"day7a-input.txt").Split(",").Select(int.Parse).ToArray();
+                int[] code5 = File.ReadAllText(@"day7a-input.txt").Split(",").Select(int.Parse).ToArray();
 
-            (bool, int) input = (false, 0);
-            int index = 0;
-            while (!input.Item1)
-            {
-                input = amplifiers[index].Process(new List<int> { input.Item2 });
-                index = ++index >= amplifiers.Count ? 0 : index;
-                Console.Write(".");
+                var inputArray = inputs.ToArray();
+
+                List<Intcode> amplifiers = new List<Intcode>
+                {
+                    new Intcode(code1).BufferInput(new List<int> {inputArray[0]}),
+                    new Intcode(code2).BufferInput(new List<int> {inputArray[1]}),
+                    new Intcode(code3).BufferInput(new List<int> {inputArray[2]}),
+                    new Intcode(code4).BufferInput(new List<int> {inputArray[3]}),
+                    new Intcode(code5).BufferInput(new List<int> {inputArray[4]})
+                };
+
+                (bool, int) input = (false, 0);
+                int index = 0;
+                while (!input.Item1)
+                {
+                    input = amplifiers[0].Process(new List<int> {input.Item2});
+                    input = amplifiers[1].Process(new List<int> {input.Item2});
+                    input = amplifiers[2].Process(new List<int> {input.Item2});
+                    input = amplifiers[3].Process(new List<int> {input.Item2});
+                    input = amplifiers[4].Process(new List<int> {input.Item2});
+                }
+
+                Console.WriteLine($"Output: {input.Item2}");
+                if (input.Item2 > highestThrust) highestThrust = input.Item2;
             }
-            Console.WriteLine($"Final output: {input.Item2}");
+
+            Console.WriteLine($"Highest thrust achieved: {highestThrust}");
         }
 
         static IEnumerable<IEnumerable<T>> GetPermutations<T>(IEnumerable<T> list, int length)
